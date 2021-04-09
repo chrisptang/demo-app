7ba8f845798a292e9da8bebb7984fda98c4fe0c5
chrisptang <chris.p.tang@gmail.com>
2021-04-09 15:16:08 +0800

    added working demo app

---
+++ Jenkinsfile
@@ -0,0 +1,25 @@
+@Library('shared-pipeline') _
+import groovy.transform.Field
+
+//应用名字；
+@Field
+String appName = 'boot-demo-app'
+
+//spring boot应用的可执行jar包模块名；
+@Field
+String mainModuleName = 'controller'
+
+//spring boot应用的HTTP端口号，请注意和其他应用区别开来；
+@Field
+String appPort = '7505'
+
+//可选项，如果没有特殊需求，可注释掉
+@Field
+String JvmOptsProd='-Xmx1g -Xms1g'
+
+//可选项，如果没有特殊需求，可注释掉
+@Field
+String JvmOptsFat='-Xmx256m -Xms256m'
+
+//这一行之后不要动：
+leqeeAdCiJava(this)
---
+++ controller/src/main/java/com/miniso/ecomm/bootdemoapp/contoller/DemoController.java
@@ -0,0 +1,29 @@
+package com.miniso.ecomm.bootdemoapp.contoller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("now", new Date());
+        model.put("requestQuery", request.getQueryString());
+        return new ModelAndView("demo", model);
+    }
+
+}
