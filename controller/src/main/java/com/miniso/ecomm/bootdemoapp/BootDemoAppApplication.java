43c4a44258c2962a6eb74f5e784c0114a2713323
chrisptang <chris.p.tang@gmail.com>
2021-04-13 12:38:14 +0800

    using miniso boot

--- controller/src/main/java/com/miniso/ecomm/bootdemoapp/BootDemoAppApplication.java
+++ controller/src/main/java/com/miniso/ecomm/bootdemoapp/BootDemoAppApplication.java
@@ -1,13 +1,15 @@
 package com.miniso.ecomm.bootdemoapp;
 
+import com.miniso.boot.autoconfiguration.annotation.EnableCat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCat
 public class BootDemoAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootDemoAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BootDemoAppApplication.class, args);
+    }
 
 }
