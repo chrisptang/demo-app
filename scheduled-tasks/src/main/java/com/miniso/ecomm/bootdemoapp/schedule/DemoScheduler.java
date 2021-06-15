9fb42be829b97f11f40f4e46136a96a8c767bcac
Tang Peng <derrick.tang@miniso.com>
2021-06-15 11:09:20 +0800

    adding xxl-job schedule demo

---
+++ scheduled-tasks/src/main/java/com/miniso/ecomm/bootdemoapp/schedule/DemoScheduler.java
@@ -0,0 +1,16 @@
+package com.miniso.ecomm.bootdemoapp.schedule;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class DemoScheduler {
+
+    @XxlJob("demoScheduler")
+    public ReturnT handleXxlJob() {
+        return new ReturnT(200, "finished at:" + new Date());
+    }
+}
