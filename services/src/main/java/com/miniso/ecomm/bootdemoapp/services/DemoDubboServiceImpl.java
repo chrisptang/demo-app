ef10dd66cd51c299082f4badb6670a249f13fe99
Tang Peng <derrick.tang@miniso.com>
2021-06-18 17:25:05 +0800

    added refrence demo

--- services/src/main/java/com/miniso/ecomm/bootdemoapp/services/DemoDubboServiceImpl.java
+++ services/src/main/java/com/miniso/ecomm/bootdemoapp/services/DemoDubboServiceImpl.java
@@ -3,10 +3,14 @@ package com.miniso.ecomm.bootdemoapp.services;
 import com.miniso.boot.client.result.Result;
 import com.miniso.ecomm.bootdemoapp.client.dto.SomeDummyObjDTO;
 import com.miniso.ecomm.bootdemoapp.client.service.DemoDubboService;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.annotation.DubboService;
 
 @DubboService
 public class DemoDubboServiceImpl implements DemoDubboService {
+
+    @DubboReference
+    private DemoDubboService demoDubboService;
 
     @Override
     public Result<String> anyService(SomeDummyObjDTO someDummyObjDTO) {
