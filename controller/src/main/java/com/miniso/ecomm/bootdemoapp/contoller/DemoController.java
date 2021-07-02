c4571f90732394935a2c2acdc30adeeddad3414c
Tang Peng <derrick.tang@miniso.com>
2021-07-02 15:39:08 +0800

    testing

--- controller/src/main/java/com/miniso/ecomm/bootdemoapp/contoller/DemoController.java
+++ controller/src/main/java/com/miniso/ecomm/bootdemoapp/contoller/DemoController.java
@@ -1,6 +1,9 @@
 package com.miniso.ecomm.bootdemoapp.contoller;
 
+import com.miniso.ecomm.apigateway.client.services.lazada.LazadaOrderService;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,20 @@ public class DemoController {
     @Autowired
     private HttpServletRequest request;
 
+    @DubboReference
+    private LazadaOrderService lazadaOrderService;
+
     @GetMapping("/index")
     public ModelAndView index() {
         Map<String, Object> model = new HashMap<>();
         model.put("now", new Date());
         model.put("requestQuery", request.getQueryString());
         return new ModelAndView("demo", model);
+    }
+
+    @GetMapping("/lazada")
+    public ResponseEntity<String> lazadaTest() {
+        return ResponseEntity.ok(lazadaOrderService
+                .getItemInfoOfOrder("", 123L).toString());
     }
 }
