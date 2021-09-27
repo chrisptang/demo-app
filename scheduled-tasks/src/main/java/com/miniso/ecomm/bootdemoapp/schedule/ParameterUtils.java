a38fd7259ee7718a80b55e72833a7340670c283c
chrisptang <chris.p.tang@gmail.com>
2021-09-27 21:35:35 +0800

    adding date-range obj

--- scheduled-tasks/src/main/java/com/miniso/ecomm/bootdemoapp/schedule/ParameterUtils.java
+++ scheduled-tasks/src/main/java/com/miniso/ecomm/bootdemoapp/schedule/ParameterUtils.java
@@ -2,6 +2,7 @@ package com.miniso.ecomm.bootdemoapp.schedule;
 
 import org.springframework.util.StringUtils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -12,8 +13,14 @@ public class ParameterUtils {
 
     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
+    private static final SimpleDateFormat SIMPLE_DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+    private static final String START_TIME_SUFFIX = " 00:00:00";
+
+    private static final String END_TIME_SUFFIX = " 23:59:59";
+
     public static String[] getDateRange(String dateRange) {
-        String toDay = SIMPLE_DATE_FORMAT.format(new Date(System.currentTimeMillis() - ONE_DAY_IN_MILLISECONDS));
+        String toDay = SIMPLE_DATE_FORMAT.format(new Date());
         String fromDay = SIMPLE_DATE_FORMAT.format(new Date(System.currentTimeMillis() - 7 * ONE_DAY_IN_MILLISECONDS));
         if (!StringUtils.isEmpty(dateRange)) {
             String[] range = dateRange.split(":");
@@ -23,5 +30,23 @@ public class ParameterUtils {
         }
 
         return new String[]{fromDay, toDay};
+    }
+
+    public static Date[] getDateRangeObj(String dateRange) {
+        Date toDay = new Date(),
+                fromDay = new Date(System.currentTimeMillis() - 7 * ONE_DAY_IN_MILLISECONDS);
+        if (!StringUtils.isEmpty(dateRange)) {
+            String[] range = dateRange.split(":");
+            if (range.length > 1) {
+                try {
+                    return new Date[]{SIMPLE_DATE_TIME_FORMAT.parse(range[0] + START_TIME_SUFFIX),
+                            SIMPLE_DATE_TIME_FORMAT.parse(range[1] + END_TIME_SUFFIX)};
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        return new Date[]{fromDay, toDay};
     }
 }
